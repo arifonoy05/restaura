0765ddb1f4310b40c0f40e748049cadc8e87b16e
jdxxmahmud <junaidmahmud@iut-dhaka.edu>
2023-10-13 18:43:45 +0100

    Security added

---
+++ security/src/test/java/com/restaura/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaura.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
